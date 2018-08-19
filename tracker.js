413d261f4f649a28d9925e410ef11b64768e8c3c
Tahshara Boochee <boocheet@gmail.com>
2018-08-19 11:08:58 -0700

    changes 8.19 with js

--- tracker.js
+++ tracker.js
@@ -2,24 +2,33 @@ let tracker = () => {
  	let min5Inc = document.getElementById('5+min')
 	let counter = document.getElementById('counter');
 	let totalTime = 0;
+  const timeInADay = 1440;
 	min5Inc.addEventListener('click', ()=>{
-		totalTime += 5;
-		counter.innerHTML = totalTime;
+    if (totalTime <= timeInADay){
+      totalTime +=5;
+      counter.innerHTML = totalTime;
+    }
 	});
   let min5Dec = document.getElementById('5-min')
 	min5Dec.addEventListener('click', ()=>{
-		totalTime -= 5;
-		counter.innerHTML = totalTime;
+    if (totalTime > 0){
+        totalTime -= 5;
+        counter.innerHTML = totalTime;
+      }
   });
   let min15Dec = document.getElementById('15-min')
 	min15Dec.addEventListener('click', ()=>{
-		totalTime -= 15;
-		counter.innerHTML = totalTime;
+    if (totalTime > 0){
+		    totalTime -= 15;
+		    counter.innerHTML = totalTime;
+      }
   });
  	let min15Inc = document.getElementById('15+min')
 	min15Inc.addEventListener('click', ()=>{
-		totalTime += 15;
-		counter.innerHTML = totalTime;
+    if (totalTime <= timeInADay){
+      totalTime += 15;
+      counter.innerHTML = totalTime;
+    }
 	});
 };
 
